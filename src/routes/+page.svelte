514e08e4cb7e0a11f5837d14c4d52198c97578b6
Brian Rono <chikoe26@gmail.com>
2023-07-31 17:31:12 +0300

    adding custom fonts and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <section>
-    <div class="nav justify-center absolute text-9xl ">
-        <img src="favicon.png" alt="logo" />
-        <p>ABOUT</p>
-        <p>CONTACT</p>
+    <div class="nav  absolute text-3xl ">
+        <ul class="list-none flex items-center  font-heebo">
+            <li class="pr-20"><img class="w-14 rounded-2xl" src="favicon.png" alt="logo" /></li>
+            <li class="pr-20 cursor-pointer"><p>ABOUT</p></li>
+            <li class="pr-20 cursor-pointer"><p>CONTACT</p></li>
+        </ul>
     </div>
 </section>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily :{
+        'heebo': "'Heebo', 'sans-serif'",
+        'edu': "'Edu SA Beginner', 'cursive'",
+      }
+      
+    },
   },
   plugins: [require("daisyui")],
 }
