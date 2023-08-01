f3b7aebe09f78f4d20f5fe461240683c5a66a3eb
Brian Rono <chikoe26@gmail.com>
2023-08-01 17:58:03 +0300

    m

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,256 @@
-<section>
-    <div class="nav  absolute text-3xl ">
+<section id="section">
+    <div class="nav  h-20 relative text-3xl ">
         <ul class="list-none flex items-center  font-heebo">
-            <li class="pr-20"><img class="w-14 rounded-2xl" src="favicon.png" alt="logo" /></li>
-            <li class="pr-20 cursor-pointer"><p>ABOUT</p></li>
-            <li class="pr-20 cursor-pointer"><p>CONTACT</p></li>
+            <li class="pr-20"><img class="w-20 rounded-2xl" src="favicon.png" alt="logo" /></li>
+            <li class="cursor-pointer"><p>ABOUT</p></li>
+            <li class="cursor-pointer"><p>CONTACT</p></li>
         </ul>
     </div>
+    <div class="home relative">
+        <img src="parent.jpg" alt="parent" class="rounded-xl" >
+        <div class="home-det">
+            <p class="heading text-6xl font-bree">Babysitting At Home</p>
+            <p class="font-ysa">Now book the babysitting at home service in 60 seconds.</p>
+            <button class="book rounded-lg bg-red-300 h-10 w-20 cursor-pointer">Book Now</button>
+        </div>
+        <div class="det pt-10">
+            <h1 class="font-bold text-4xl mt-7 relative mx-24">Find Babysitters & Nannies - Babysitting Service at Home</h1>
+            <p class="pt-10 relative mx-24 pb-7 text-xl">It is very important for parents to find the best nanny possible for their little ones. The family's home is where the babies and young children are cared for by a qualified nanny. Designated as a <b>professional babysitter</b>, she performs her childcare duties, gives each family member's child individual attention, supports kids with active learning tasks and social activities, and assists moms in all areas. At Justlife, you will find the best <b>babysitter wanted</b> in your area, easily. Professional <b>babysitting services</b> care for kids of all ages, from infants to those in school, and can provide tailored services for particular age ranges or kids with special needs. Based on the agreement with the parents, the <b>nanny service</b> we offer will aid your child's development and help you find some free time for yourself.</p>
+            <p class="pt-10 relative mx-24 pb-7 text-xl"> A babysitter typically provides day-to-day, part-time, or full-time care for the child while they are at home. Typical babysitter tasks consist of:</p>
+            <ul class="pt-10 relative mx-24 pb-7 text-xl list-disc">
+                <li>Assistance and supervision of children at the workplace</li>
+                <li>Preparing infant nutrition products</li>
+                <li>Preparing a variety of snacks for kids</li>
+                <li>Preparing food for bottle-fed children</li>
+                <li>Supporting education</li>
+                <li>Getting ready</li>
+                <li>Baby toilet training and diaper changes</li>
+                <li>Know the fundamentals, and assist with homework </li>
+            </ul>
+            <p class="pt-10 relative mx-24 pb-7 text-xl">And many more or </p>
+        </div>
+    </div>
+    <div class="play pt-10 relative">
+        <img src="blocks.JPG" alt="blocks" class="relative rounded-xl">
+        <div class="play-det absolute">
+            <p class="heading-p text-6xl font-bree">Babysitting At Our Facilities</p>
+            <p class="font-ysa">Now book a session for your baby at our facilities.</p>
+            <button class="book-p rounded-lg bg-red-300 h-10 w-20 cursor-pointer">Book Now</button>
+        </div>
+        <div class="det-p pt-10">
+            <h1 class="font-bold text-4xl mt-7 relative mx-24">Allow Your Child To Play And Grow With Us Here At Salama Baby Sitters</h1>
+            <p class="pt-10 relative mx-24 pb-7 text-xl">
+                Interacting with other children helps kids build <b>social skills, empathy, and emotional intelligence</b>. They learn how to share, take turns, cooperate, and negotiate, which are crucial for forming healthy relationships later in life.Positive social interactions and successful play experiences with others can boost a child's self-confidence and self-esteem. Feeling accepted and valued by peers contributes to a healthy self-concept.
+            </p>
+            <p class="pt-10 relative mx-24 pb-7 text-xl"> Depending on the situation and the parents’ consent, babysitting  may also help with other parental duties. Among them are;</p>
+            <ul class="pt-10 relative mx-24 pb-7 text-xl list-disc">
+                <li>Keeping the kitchen clean</li>
+                <li>Preparing the king beds</li>
+                <li>Washing the family's clothes</li>
+                <li>Washing towels and bed linens</li>
+                <li>Preparing dinner for the family</li>
+                <li>Cleaning up the house.</li>
+                <li>Purchasing supplies or clothing for the kids</li>
+                <li>Kitchen cabinet organization</li>
+            </ul>
+            <h1 class="font-bold text-4xl mt-7 relative mx-24">Why Should You Take Babysitting Services From Salama Professional Babysitters?</h1>
+            <p class="pt-10 relative mx-24 pb-7 text-xl">An excellent babysitter should be patient, empathetic, peaceful, sociable, truthful, loving children, heartfelt, reliable, and totally open to interaction. When you decide that you are searching for a babysitter, we send you people who are properly competent for doing their job, who have received the necessary training, and who genuinely enjoy doing this important job of babysitting. <br> We are aware of how much you adore your child and are willing to do anything for them. We are here to help you because we want to make your life simpler and bring you some relief. <br>In an emergency, a qualified and trained babysitter should remain composed and think of the best options for your child. At this spot, expert babysitters should also have training and knowledge in child development. We at Justlife's Professional Babysitters only hire well-trained, compassionate individuals to be your child's nanny. We believe you won't regret picking a nanny from a pool of gifted individuals who can develop a loving relationship with children.</p>
+            <h1 class="font-bold text-4xl mt-7 relative mx-24">How to Find the Best Babysitter/Nanny for You?</h1>
+            <p class="pt-10 relative mx-24 pb-7 text-xl">
+                You can find a babysitter easily. All of our caregivers are skilled and experienced. They are friendly, dependable, and totally open to interaction. We hire professional caregivers who love kids above all else in order to provide you with the best service. <br>All of our nannies have childcare expertise, are trained in pediatric first aid, and are confirmed as caregivers. They have the skills necessary to take care of kids of all ages, even newborns. You can get in touch with us so that we can give your child the best care possible.
+            </p>
+            <h1 class="font-bold text-4xl mt-7 relative mx-24">How Much Is The Babysitting Service at Home?</h1>
+            <p class="pt-10 relative mx-24 pb-7 text-xl">
+                Your child means the world to you, as we all know. Therefore, we aim to give you and your child the chance to improve the quality of your lives. Our <b>babysitting prices</b> are set depending on the period of babysitting. You can check our website or app to view them.
+            </p>
+        </div>
+    </div>
+    <div class="services">
+        <h1 class="font-bold text-4xl mt-7 relative mx-24">What other services can we help you with?</h1>
+        <div class="services-det relative h-96 my-10">
+            <div class="barbershop rounded-xl absolute bg-gray-500 h-72 w-44 mt-7">
+                <div class="img rounded-full h-20 w-20  bg-white">
+                    <img src="barbershop.png" alt="barber" class=" p-2  ">
+                </div>
+                <div class="barber-det">
+                    <h2><b>MEN'S SALON</b></h2>
+                    <p>Exceptional curated salon experience at home and our facilities</p>
+                    <button class="bg-blue-500 text-white h-7 cursor-pointer rounded-2xl w-28">BOOK NOW</button>
+                </div>
+                
+            </div>
+            <div class="hairsalon rounded-xl absolute bg-gray-500 h-72 w-44 mt-7">
+                <div class="img rounded-full h-20 w-20  bg-white">
+                        <img src="hair-salon.png" alt="barber" class=" p-2 ">
+                </div>
+                <h2><b>WOMEN'S SALON</b></h2>
+                <p>Book beauty services at the comfort of your home and at our facilities</p>
+                <button class="bg-blue-500 text-white h-7 cursor-pointer rounded-2xl w-28">BOOK NOW</button>
+            </div>
+            <div class="nail rounded-xl absolute bg-gray-500 h-72 w-44 mt-7">
+                <div class="img rounded-full h-20 w-20 bg-white">
+                    <img src="nail-polish.png" alt="barber" class=" p-2 ">
+                </div>
+                <h2><b>NAIL COUTURE</b></h2>
+                <p>Get luxury nail-care from nail art,to arcrylic extensions and more premium nail care</p>
+                <button class="bg-blue-500 text-white h-7 cursor-pointer rounded-2xl w-28">BOOK NOW</button>
+            </div>
+            <div class="baby-sitting rounded-xl absolute bg-gray-500 h-72 w-44 mt-7">
+                <div class="img rounded-full h-20 w-20 bg-white">
+                    <img src="baby-girl.png" alt="barber" class=" p-2 ">
+                </div>
+                <h2><b>BABY SITTING</b></h2>
+                <p>Even super Moms & Dads need an extra pair of hands</p>
+                <button class="bg-blue-500 text-white h-7 cursor-pointer rounded-2xl w-28">BOOK NOW</button>
+            </div>
+            <div class="therapy rounded-xl absolute bg-gray-500 h-72 w-44 mt-7">
+                <div class="img rounded-full h-20 w-20 bg-white">
+                    <img src="therapies.png" alt="barber" class=" p-2 ">
+                </div>
+                <h2><b>ONLINE THERAPY</b></h2>
+                <p>Online therapy with top-rated psychologists</p>
+                <button class="bg-blue-500 text-white h-7 cursor-pointer rounded-2xl w-28">BOOK NOW</button>
+            </div>
+            <div class="spa rounded-xl absolute bg-gray-500 h-72 w-44 mt-7">
+                <div class="img rounded-full h-20 w-20 bg-white">
+                    <img src="facial.png" alt="barber" class=" p-2 ">
+                </div>
+                <h2><b>WOMEN'S SPA</b></h2>
+                <p>Indulgent luxury for mind, body, and soul</p>
+                <button class="bg-blue-500 text-white h-7 cursor-pointer rounded-2xl w-28">BOOK NOW</button>
+            </div>  
+        </div>
+    </div>
+    <div class="about h-72  bg-gray-700 relative">
+        <div class="links absolute">
+            <h2><b>Useful links</b></h2>
+            <ul>
+                <li><a href="/">Learn More About Us</a></li>
+                <li><a href="/">Baby Sitting At Home</a></li>
+                <li><a href="/">Spa services</a></li>
+                <li><a href="/">Hair Services</a></li>
+                <li><a href="/">Online Therapy</a></li>
+            </ul>
+        </div>
+        <div class="contact absolute">
+            <h2><b>Contact Us</b></h2>
+            <p><b>Address:</b></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p><b>Phone:</b></p>
+            <p>0722000000</p>
+            <p><b>Email:</b></p>
+            <a href="/">salamababysitters@outlook.com</a>
+
+
+        </div>
+
+    </div>
+
 </section>
 
 <style lang="postcss">
     :global(html) {
-      background-color: theme(colors.gray.100);
+    background-color: theme(colors.gray.100);
+      
     }
+    #section{
+        width: 100vw;
+    }
+    .play img{
+        width: 80%;
+        left: 10%;
+        height: 600px;
+        filter: brightness(40%);
+    }
+    body{
+        width: 100%;
+    }
+    .play .play-det{
+        color: white;
+        position: absolute;
+        bottom: 75%;
+        left: 12%;
+    }
+    .home img{
+        position: relative;
+        left: 10%;
+        width: 80%;
+        height: 500px;
+        filter: brightness(40%);
+    }
+    .home .home-det{
+        color: white;
+        position: absolute;
+        bottom: 69%;
+        left: 12%;
+    }
+    .nav {
+        width: 100%;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        margin-bottom: 10px;
+    }
+    .parent{
+        color: black;
+    }
+    .parent img {
+        width:70%;
+        height: 400px;
+        left: 10%;
+    }
+    .about .links{
+        left: 10%;
+        top: 15%;
+    }
+    .about .contact{
+        right: 10%;
+        top: 15%;
+    }
+    .services-det .barbershop{
+        left: 3%;
+    }
+    .services-det .hairsalon{
+        left: 19%;
+    }
+    .services-det .nail{
+        left: 35%;
+    }
+    .services-det .baby-sitting{
+        left: 51%;
+    }
+    .services-det .therapy{
+        left: 67%;
+    }
+    .services-det .spa{
+        left: 83%;
+    }
+    .services .services-det button{
+        position: absolute;
+        bottom: 2%;
+        left: 20%;
+    }
+    .services .services-det .barber-det{}
+
+    .services .services-det p{
+        position:relative;
+        margin-left: 8px; /* Set the left margin to 20 pixels */
+        margin-right: 8px;
+        display: flex; /* Use flexbox */
+        justify-content: center; /* Horizontally center the text */
+        align-items: center;
+    }
+    .services .services-det h2{
+        display: flex; /* Use flexbox */
+        justify-content: center; /* Horizontally center the text */
+        align-items: center;
+        padding-bottom: 5px;
+    }
+    .services .services-det .img{
+        position: relative;
+        left: 25%;
+        top: 2%;
+        margin-bottom: 20px;
+    }
+    
   </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,17 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
+      height :{
+        'h': "200px"
+      },
+      backgroundImage: {
+        'parent': "url('parent.jpg')",
+      },
       fontFamily :{
         'heebo': "'Heebo', 'sans-serif'",
         'edu': "'Edu SA Beginner', 'cursive'",
+        'ysa':"'Ysabeau Infant', 'sans-serif'",
+        'bree':"'Patua One', 'serif'",
       }
       
     },
